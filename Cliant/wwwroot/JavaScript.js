d812fcd5130f8d41f167a325035285a3319e33e7
TACT21 <takumokumoku@gmail.com>
2022-06-23 21:45:13 +0900

    Add memory helper

--- Cliant/wwwroot/JavaScript.js
+++ Cliant/wwwroot/JavaScript.js
@@ -1,20 +1,98 @@
 ﻿console.log("Loading..");
 
-window.addEventListener('message', function (e) {
-    //if (event.origin !== "https://example.com") 
-        //return;
-    switch (e.data.action) {
-        case 'googleLogin':
-            console.log(e.data.message);
-            break;
-    }
-});
+var value = "";
+var isComp = false;
+var isUsed = false;
+var body = "NULL";
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
-var isComp = false;
-var body = "NULL";
+
+function ifream(id) {
+    const parent = document.getElementsByTagName("body")[0];
+
+    let img = document.createElement('ifream');
+    img.id = 'img-class';
+    img.src = 'sample.jpg';
+
+    parent.appendChild(img);
+}
+
+async function GetValue(valueTag, settimeout) {
+    if (isUsed) {
+        throw;
+    }
+    isUsed = true;
+    var iframe = document.getElementsByName('iframe')[0];
+    iframe.postMessage({
+        action: 'CallValue',
+        message: valueTag
+    }, '*');
+    for (var i = 0; i < (settimeout / 500); i++) {
+        await sleep(500);
+        if (isComp) {
+            break;
+        }
+    }
+    isComp = false;
+    isUsed = false;
+    return value;
+}
+
+function ToLocal(tag, value, shareOrigin, shareAddress) {
+    var iframe = document.getElementsByName('iframe')[0];
+    iframe.postMessage({
+        action: 'SetValue',
+        tag: tag,
+        message: value,
+        Origin: shareOrigin
+               adress: shareAddress
+    }, '*');
+}
+
+function ToSession(value, shareOrigin, shareAddress) {
+    var iframe = document.getElementsByName('iframe')[0];
+    iframe.postMessage({
+        action: 'SendValue',
+        tag: tag,
+        message: value,
+        Origin: shareOrigin,
+        adress: shareAddress
+    }, '*');
+}
+
+async function TrySet(type, value, shareOrigin, shareAddress, timeout = 5000) {
+    var respons = "";
+    if (isUsed) {
+        throw;
+    }
+    isUsed = true;
+    if (type == "Local") {
+        ToLocal(value, shareOrigin, shareAddress)
+    } else if (type == "Session") {
+        ToSession(value, shareOrigin, shareAddress)
+    }
+    for (var i = 0; i < (out / 500); i++) {
+        await sleep(500);
+        if (isComp) {
+            respons = body;
+            break;
+        }
+    }
+    isUsed = false;
+    isComp = false;
+    return respons;
+}
+
+window.addEventListener('message', function (e) {
+    switch (e.data.action) {
+        case 'ReturnValue':
+            isComp = true;
+            value = e.data.message;
+            break;
+    }
+});
 
 async function People(url, out = 10000) {
     window.open(url, "child_window", "width=500,height=750,scrollbars=yes");
