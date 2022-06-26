3110f47bca09a7dfb2ffa26a456b184a8af3ae78
TACT21 <takumokumoku@gmail.com>
2022-06-26 21:07:26 +0900

    adaptation helper test to helper ver 0.2.1

--- Cliant/wwwroot/test.js
+++ Cliant/wwwroot/test.js
@@ -17,90 +17,32 @@ window.convertArray = (win1251Array) => {
     return decodedArray;
 };
 
-window.ifream = (id) => {
-    console.log("called")
-    const parent = document.getElementsByTagName("article")[0];
-    let img = document.createElement('ifream');
-    img.id = id;
-    img.src = "https://tact21.github.io/Cluture_test/Shareing/";
-    console.log("Add!")
-    parent.appendChild(img);
-}
-
-async function GetValue(valueTag, settimeout) {
-    if (isUsed) {
-        throw  'another task use memory helper'
-    }
-    isUsed = true;
-    var iframe = document.getElementsByName('iframe')[0];
-    iframe.postMessage({
-        action: 'CallValue',
-        message: valueTag
-    }, '*');
-    for (var i = 0; i < (settimeout / 500); i++) {
-        await sleep(500);
-        if (isComp) {
-            break;
-        }
-    }
-    isComp = false;
-    isUsed = false;
-    return value;
-}
-
-function ToLocal(tag, value, shareOrigin, shareAddress) {
-    var iframe = document.getElementsByName('iframe')[0];
-    iframe.postMessage({
-        action: 'SetValue',
-        tag: tag,
-        message: value,
-        Origin: shareOrigin,
-        adress: shareAddress
-    }, '*');
-}
-
-function ToSession(value, shareOrigin, shareAddress) {
-    var iframe = document.getElementsByName('iframe')[0];
-    iframe.postMessage({
-        action: 'SendValue',
-        tag: tag,
-        message: value,
-        Origin: shareOrigin,
-        adress: shareAddress
-    }, '*');
-}
-
-async function TrySet(type, value, shareOrigin, shareAddress, timeout = 5000) {
-    var respons = "";
-    if (isUsed) {
-        throw  'another task use memory helper'
-    }
-    isUsed = true;
-    if (type == "Local") {
-        ToLocal(value, shareOrigin, shareAddress)
-    } else if (type == "Session") {
-        ToSession(value, shareOrigin, shareAddress)
-    }
-    for (var i = 0; i < (out / 500); i++) {
-        await sleep(500);
-        if (isComp) {
-            respons = body;
-            break;
-        }
-    }
-    isUsed = false;
-    isComp = false;
-    return respons;
-}
-
-window.addEventListener('message', function (e) {
-    switch (e.data.action) {
-        case 'ReturnValue':
-            isComp = true;
-            value = e.data.message;
-            break;
-    }
+window.addEventListener('message', function (event) {
+    body = event.data;
+    alert(event.data);
 });
+
+async function Get(tag, id) {
+    var unit = 500
+    document.querySelector('#' + id).contentWindow.postMessage('get' + ',' + tag);
+    for (var i = 0; i < (5000 / unit); i++) {
+        sleep(unit);
+        if (body != "NULL") {
+            break;
+        }
+    }
+    var result = body;
+    body = "NULL";
+    return result
+}
+
+function Del(tag,id) {
+    document.querySelector('#' + id).contentWindow.postMessage('del'+','+ tag);
+}
+
+function Set(tag,content, id) {
+    document.querySelector('#' + id).contentWindow.postMessage('get'+','+ tag+','+ content);
+}
 
 async function People(url, out = 10000) {
     window.open(url, "child_window", "width=500,height=750,scrollbars=yes");
