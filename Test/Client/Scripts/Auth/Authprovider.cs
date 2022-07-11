c4f28f649a28b33444ba6070ae9b849046607a05
TACT21 <53495996+TACT21@users.noreply.github.com>
2022-07-11 17:24:43 +0900

    add login Scripts

---
+++ Test/Client/Scripts/Auth/Authprovider.cs
@@ -0,0 +1,131 @@
+﻿using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Authentication;
+using System.Threading.Tasks;
+using Test.Shared.Services.Auth;
+
+namespace Test.Client.Scripts.Auth
+{
+    public class Authprovider : IAuthService
+    {
+        public enum LoginMode
+        {
+            Mictrosoft = 0,
+            Google
+        }
+
+        public LoginMode loginMode;
+        async Task<LoginResult> LoginAsync(LoginModel loginModel)
+        {
+            if(((int)loginMode) == 0)
+            {
+                return await MicrosoftLogin(loginModel);
+            }else if(((int)loginMode) == 1)
+            {
+                return await GoogleLogin(loginModel);
+            }
+            else
+            {
+                throw new Exception("Unexpect Mode");
+            }
+        }
+
+        private readonly AuthenticationStateProvider _authenticationStateProvider;
+        public Authprovider(AuthenticationStateProvider authenticationStateProvider)
+        {
+            _authenticationStateProvider = authenticationStateProvider;
+        }
+
+        public async Task<LoginResult> MicrosoftLogin(LoginModel loginModel)
+        {
+            // wait 3 seconds
+            await Task.Delay(3000);
+            if (loginModel == null)
+            {
+                throw new ArgumentException("loginModel is null");
+            }
+            if (loginModel.UserID == "demo" && loginModel.Password == "demo")
+            {
+                var res = new LoginResult()
+                {
+                    IsSuccessful = true,
+                    IDToken = "hoge"
+                };
+                var roles = new List<string>();
+                roles.Add("Admin");
+                await ((SPAAuthticateProvider)_authenticationStateProvider).MarkUserAsAuthenticated(loginModel.UserID, res.IDToken, roles);
+                return res;
+            }
+            else if (loginModel.UserID == "demo2" && loginModel.Password == "demo2")
+            {
+                var res = new LoginResult()
+                {
+                    IsSuccessful = true,
+                    IDToken = "hoge"
+                };
+                var roles = new List<string>();
+                roles.Add("User");
+                await ((SPAAuthticateProvider)_authenticationStateProvider).MarkUserAsAuthenticated(loginModel.UserID, res.IDToken, roles);
+                return res;
+            }
+            else
+            {
+                return new LoginResult()
+                {
+                    IsSuccessful = false,
+                    Error = new AuthenticationException("NotAuthrized")
+                };
+            }
+        }
+
+        public async Task<LoginResult> GoogleLogin(LoginModel loginModel)
+        {
+            // wait 3 seconds
+            await Task.Delay(3000);
+            if (loginModel == null)
+            {
+                throw new ArgumentException("loginModel is null");
+            }
+            if (loginModel.UserID == "demo" && loginModel.Password == "demo")
+            {
+                var res = new LoginResult()
+                {
+                    IsSuccessful = true,
+                    IDToken = "hoge"
+                };
+                var roles = new List<string>();
+                roles.Add("Admin");
+                await ((SPAAuthticateProvider)_authenticationStateProvider).MarkUserAsAuthenticated(loginModel.UserID, res.IDToken, roles);
+                return res;
+            }
+            else if (loginModel.UserID == "demo2" && loginModel.Password == "demo2")
+            {
+                var res = new LoginResult()
+                {
+                    IsSuccessful = true,
+                    IDToken = "hoge"
+                };
+                var roles = new List<string>();
+                roles.Add("User");
+                await ((SPAAuthticateProvider)_authenticationStateProvider).MarkUserAsAuthenticated(loginModel.UserID, res.IDToken, roles);
+                return res;
+            }
+            else
+            {
+                return new LoginResult()
+                {
+                    IsSuccessful = false,
+                    Error = new AuthenticationException("NotAuthrized")
+                };
+            }
+        }
+
+        public async Task LogoutAsync()
+        {
+            await ((SPAAuthticateProvider)_authenticationStateProvider).MarkUserAsLoggedOut();
+        }
+    }
+}
---
+++ Test/Client/Scripts/Auth/authticate_provider.cs
@@ -0,0 +1,71 @@
+﻿using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Test.Client.Scripts.Auth
+{
+    public class SPAAuthticateProvider : AuthenticationStateProvider
+    {
+        private readonly HttpClient _httpClient;
+        private readonly ILocalStorageService _localStorage;
+
+        public SPAAuthticateProvider(HttpClient httpClient, ILocalStorageService localStorage)
+        {
+            _httpClient = httpClient;
+            _localStorage = localStorage;
+        }
+        public async override Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            var savedToken = await _localStorage.GetItemAsync<string>("authToken");
+            var userID = await _localStorage.GetItemAsync<string>("userID");
+            var roles = await _localStorage.GetItemAsync<List<string>>("roles");
+            // do token check if it is needed
+
+            if (string.IsNullOrWhiteSpace(savedToken))
+            {
+                return new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity()));
+            }
+
+            var claims = new List<Claim>();
+            claims.Add(new Claim(ClaimTypes.Name, userID));
+
+            foreach (var role in roles)
+            {
+                claims.Add(new Claim(ClaimTypes.Role, role));
+            }
+
+            // HTTPの認証用のトークンを設定
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", savedToken);
+
+            return new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity(claims, "User")));
+        }
+
+        public async Task MarkUserAsAuthenticated(string userID, string authToken, List<string> roles = null)
+        {
+            await _localStorage.SetItemAsync("userID", userID);
+            await _localStorage.SetItemAsync("authToken", authToken);
+            if (roles != null)
+            {
+                await _localStorage.SetItemAsync("roles", roles);
+            }
+            NotifyAuthenticationStateChanged(GetAuthenticationStateAsync());
+        }
+
+        public async Task MarkUserAsLoggedOut()
+        {
+            await _localStorage.RemoveItemAsync("userID");
+            await _localStorage.RemoveItemAsync("authToken");
+            await _localStorage.RemoveItemAsync("roles");
+            if (_httpClient.DefaultRequestHeaders.Authorization != null)
+            {
+                _httpClient.DefaultRequestHeaders.Authorization = null;
+            }
+            NotifyAuthenticationStateChanged(GetAuthenticationStateAsync());
+        }
+    }
+}
