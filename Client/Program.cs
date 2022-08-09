af5031e523c2cce52cb622a538847213b52940d4
TACT21 <takumokumoku@gmail.com>
2022-08-09 22:58:10 +0900

    Add error parts

--- Client/Program.cs
+++ Client/Program.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.JSInterop;
 using bluem_of_youth.Client;
 
+
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
@@ -12,11 +13,15 @@ builder.Services.AddHttpClient("bluem_of_youth.ServerAPI", client => client.Base
     .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
 // Supply HttpClient instances that include access tokens when making requests to the server project
 builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("bluem_of_youth.ServerAPI"));
-
+IConfiguration configuration = new ConfigurationBuilder()
+.SetBasePath(Directory.GetCurrentDirectory())
+.AddJsonFile("appsettings.json", true, true)
+.Build();
+IConfigurationSection section = configuration.GetSection("AzureAd");
 builder.Services.AddMsalAuthentication(options =>
 {
     builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
-    options.ProviderOptions.DefaultAccessTokenScopes.Add("api://453fa4c2-fc1a-45be-ba70-7c7dd5070f7a/access_as_user");
+    options.ProviderOptions.DefaultAccessTokenScopes.Add($"api://{section["ClientId"]}/access_as_user");
     //options.ProviderOptions.DefaultAccessTokenScopes.Add("api://api.id.uri/access_as_user");
     options.ProviderOptions.DefaultAccessTokenScopes.Add("https://graph.microsoft.com/User.Read");
     options.ProviderOptions.LoginMode = "redirect";
