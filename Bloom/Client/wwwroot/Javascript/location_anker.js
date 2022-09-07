66d2d29f9080e85bdbbfb3454c90aee43887bb51
TACT21 <takumokumoku@gmail.com>
2022-09-07 22:40:46 +0900

    to use server

---
+++ Bloom/Client/wwwroot/Javascript/location_anker.js
@@ -0,0 +1,12 @@
+﻿function Org() {
+    var result = "commite"
+    if (location.href.indexOf("club") != -1) {
+        result = "club"
+    } else if (location.href.indexOf("org") != -1) {
+        result = "org"
+    } else if (location.href.indexOf("shool") != -1) {
+        result = "shool"
+    }
+
+    return result;
+}
---
+++ Bloom/Client/wwwroot/Javascript/navigation_helper.js
@@ -0,0 +1,20 @@
+﻿window.scrollToElementId = (elementId) => {
+    console.info('scrolling to element', elementId);
+    var element = document.getElementById(elementId);
+    if (!element) {
+        console.warn('element was not found', elementId);
+        return false;
+    }
+    var new_element = document.createElement('div');
+    new_element.style.position = "relative";
+    new_element.style.top = "-5vh";
+    new_element.style.height = "1px";
+    element.before(new_element);
+    new_element.scrollIntoView();
+    new_element.remove();
+    return true;
+}
+
+function jumpto(url) {
+    location.href = url;
+}
